a1983edd1d710ae8e82d05b65d3dc04f56cc7bd3
Xavier Paez <xavipaez2000@hotmail.com>
2022-08-08 12:14:38 -0500

    TAREA 27: JOIN WHERE y JOIN FETCH en JPA

--- src/main/java/com/uce/edu/demo/ProyectoU3XpApplication.java
+++ src/main/java/com/uce/edu/demo/ProyectoU3XpApplication.java
@@ -9,18 +9,15 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.uce.edu.demo.repository.modelo.DetalleFactura;
 import com.uce.edu.demo.repository.modelo.Factura;
-import com.uce.edu.demo.repository.modelo.Habitacion;
-import com.uce.edu.demo.repository.modelo.Hotel;
 import com.uce.edu.demo.service.IFacturaService;
-import com.uce.edu.demo.service.IHotelService;
 
 @SpringBootApplication
 public class ProyectoU3XpApplication implements CommandLineRunner {
 
 	private static final Logger LOG = LoggerFactory.getLogger(ProyectoU3XpApplication.class);
-	@Autowired
-	private IHotelService hotelService;
+
 	@Autowired
 	private IFacturaService facturaService;
 
@@ -32,26 +29,21 @@ public class ProyectoU3XpApplication implements CommandLineRunner {
 	public void run(String... args) throws Exception {
 		// TODO Auto-generated metfod stub
 
-		List<Factura> lista = this.facturaService.buscarFacturaInnerJoin(2);
-		for (Factura factura : lista) {
-			LOG.info("Factura: " +  factura.getNumero() + " Fecha: " + factura.getFecha());
+		LOG.info("RELACIONAMIENTO WHERE");
+		List<Factura> listaFactura = this.facturaService.buscarFacturaJoinWhere(2);
+		for (Factura f : listaFactura) {
+			LOG.info("Factura: " + f.getNumero() + " " + f.getFecha());
 		}
 
-		List<Factura> lista2 = this.facturaService.buscarInnerJoin();
-		for (Factura factura : lista2) {
-			LOG.info("Factura2: " + factura.getNumero() + " Fecha: " + factura.getFecha());
-		}
-
-		List<Factura> listaLeft = this.facturaService.buscarFacturaOuterJoinLeft(2);
-		for (Factura factura : listaLeft) {
-			LOG.info("Factura Left: " + factura.getNumero() + " Fecha: " + factura.getFecha());
-		}
 	
-		List<Factura> listaRight = this.facturaService.buscarFacturaOuterJoinRight(2);
-		for (Factura factura : listaRight) {
-			LOG.info("Factura Right: " + factura.getNumero() + " Fecha: " + factura.getFecha());
+		LOG.info("JOIN FETCH");
+		List<Factura> listaFactura3 = this.facturaService.buscarFacturaFetchJoin(2);
+		for (Factura f : listaFactura3) {
+			LOG.info("Factura: " + f.getNumero() + " " + f.getFecha());
+			for (DetalleFactura  df: f.getDetalles()) {
+				LOG.info("Detalle: " + df);
+			}
+
 		}
-
-
-	}
+}
 }
--- src/main/java/com/uce/edu/demo/repository/FacturaRepositoryImpl.java
+++ src/main/java/com/uce/edu/demo/repository/FacturaRepositoryImpl.java
@@ -10,6 +10,7 @@ import javax.transaction.Transactional;
 import org.springframework.stereotype.Repository;
 
 import com.uce.edu.demo.repository.modelo.Factura;
+import com.uce.edu.demo.repository.modelo.Hotel;
 
 @Repository
 @Transactional
@@ -56,13 +57,18 @@ public class FacturaRepositoryImpl implements IFacturaRepository {
 	@Override
 	public List<Factura> buscarFacturaJoinWhere(Integer cantidad) {
 		// TODO Auto-generated method stub
-		return null;
+		TypedQuery<Factura> myQuery = this.entityManager.createQuery(
+				"SELECT f FROM Factura f, DetalleFactura df WHERE f = df.factura AND df.cantidad = :cantidad", Factura.class);
+		myQuery.setParameter("cantidad", cantidad);
+		return myQuery.getResultList();
 	}
 
 	@Override
 	public List<Factura> buscarFacturaFetchJoin(Integer cantidad) {
 		// TODO Auto-generated method stub
-		return null;
+		TypedQuery<Factura> myQuery=this.entityManager.createQuery("SELECT f FROM Factura f JOIN FETCH f.detalles df WHERE df.cantidad =:cantidad", Factura.class);
+		myQuery.setParameter("cantidad", cantidad);
+		return myQuery.getResultList();
 	}
 
 }
