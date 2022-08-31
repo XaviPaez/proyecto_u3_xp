65c541fbaaa0febddcb77557484f74424cc2f887
Xavier Paez <xavipaez2000@hotmail.com>
2022-08-30 20:22:41 -0500

    TALLER 36 STREAM PARALLELSTREAM

--- src/main/java/com/uce/edu/demo/hilo/tradicional/Main.java
+++ src/main/java/com/uce/edu/demo/hilo/tradicional/Main.java
@@ -7,21 +7,25 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		long tiempoInicial= System.currentTimeMillis();
-		Cajero cajero1=new Cajero("Pablo", Arrays.asList("Pepito","Juan","Daniel","Pedro"));
-		Cajero cajero2=new Cajero("Josue", Arrays.asList("Pepito2","Juan2","Daniel2"));
-		Cajero cajero3=new Cajero("Mike", Arrays.asList("Pepito3","Juan3"));
+		long tiempoInicial = System.currentTimeMillis();
 
-		PCCajero gestorAtencion=new PCCajero();
+		System.out.println("Nombre hilo: " + Thread.currentThread().getName());
+		Cajero cajero1 = new Cajero("Pablo", Arrays.asList("Pepito", "Juan", "Daniel", "Pedro"));
+		Cajero cajero2 = new Cajero("Josue", Arrays.asList("Pepito2", "Juan2", "Daniel2"));
+		Cajero cajero3 = new Cajero("Mike", Arrays.asList("Pepito3", "Juan3"));
+
+		PCCajero gestorAtencion = new PCCajero();
 		gestorAtencion.procesar(cajero1);
-		gestorAtencion.procesar(cajero2);
-		gestorAtencion.procesar(cajero3);
+		PCCajero gestorAtencion1 = new PCCajero();
+		gestorAtencion1.procesar(cajero2);
+		PCCajero gestorAtencion2 = new PCCajero();
+		gestorAtencion2.procesar(cajero3);
 
-		long tiempoFinal= System.currentTimeMillis();
+		long tiempoFinal = System.currentTimeMillis();
 
-		long tiempoTranscurrido=(tiempoFinal-tiempoInicial)/1000;
+		long tiempoTranscurrido = (tiempoFinal - tiempoInicial) / 1000;
 
-		System.out.println(tiempoTranscurrido+" seg");
+		System.out.println(tiempoTranscurrido + " seg");
 	}
 
 }
